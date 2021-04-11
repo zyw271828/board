481881e7393b42920f47ff71bcf43d89f90dee30
zyw271828 <zyw_0618@outlook.com>
2021-04-11 20:53:52 +0800

    Add LocalStorageService

---
+++ lib/services/local_storage_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:board/models/note.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorageService {
+  static loadNote() async {
+    List<Note> notes;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int numberOfNotes = prefs.getInt('numberOfNotes');
+
+    if (numberOfNotes != null) {
+      for (var i = 0; i < numberOfNotes; i++) {
+        String jsonString = prefs.getString('note' + i.toString());
+        Map noteMap = jsonDecode(jsonString);
+        var note = Note.fromJson(noteMap);
+        notes.add(note);
+      }
+    } else {
+      notes = <Note>[
+        Note('Note A', 900),
+        Note('Note B', 800),
+        Note('Note C', 700),
+        Note('Note D', 600),
+        Note('Note E', 500),
+        Note('Note F', 400),
+      ];
+    }
+    return notes;
+  }
+
+  static saveNote(List<Note> notes) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int numberOfNotes = notes.length;
+    prefs.setInt('numberOfNotes', numberOfNotes);
+
+    for (var i = 0; i < numberOfNotes; i++) {
+      String json = jsonEncode(notes[i]);
+      prefs.setString('note' + i.toString(), json);
+    }
+  }
+}
