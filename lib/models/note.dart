677a4fa0fa990191bbea37c31fedfd377867b058
zyw271828 <zyw_0618@outlook.com>
2021-04-11 20:42:32 +0800

    Implement note serialization

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -3,4 +3,13 @@ class Note {
   int colorCode;
 
   Note(this.content, this.colorCode);
+
+  Note.fromJson(Map<String, dynamic> json)
+      : content = json['content'],
+        colorCode = json['colorCode'];
+
+  Map<String, dynamic> toJson() => {
+        'content': content,
+        'colorCode': colorCode,
+      };
 }
